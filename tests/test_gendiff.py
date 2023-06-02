f06c90b2a9476d1f3c50d8251569be9d42ce4270
Vlasova Nika <vlasova_nika69@mail.ru>
2023-06-03 01:40:35 +0300

    add tests/fixtures/right_result.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,5 @@
 from gendiff.generate import generate_diff
-from fixtures.right_result import RIGHT_RESULT
+from .fixtures.right_result import RIGHT_RESULT
 
 def test_generate_diff():
-    
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == RIGHT_RESULT
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
