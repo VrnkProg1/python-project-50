519966d29e1dfe5e27499b083ffca7e382e181c6
Vlasova Nika <vlasova_nika69@mail.ru>
2023-06-03 13:17:38 +0300

    add test for .yaml .yml

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,3 +3,13 @@ from .fixtures.right_result import RIGHT_RESULT
 
 def test_generate_diff():
     assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml')
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')
+
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.yml')
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.yaml')
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.json')
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.json')
+
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml')
+    assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yaml')
