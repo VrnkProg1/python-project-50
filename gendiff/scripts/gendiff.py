8ace8750a03d7cb6b07e1cc9deba4029db972ee9
Vlasova Nika <vlasova_nika69@mail.ru>
2023-07-23 14:41:10 +0300

    add .

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""Gendiff script."""
+
+import argparse
+
+from gendiff import generate_diff
+
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        default = 'stylish')
+    args = parser.parse_args()
+
+
+print(generate_diff(args.first_file, args.second_file, args.format))
+
+
+if __name__ == '__main__':
+    main()
