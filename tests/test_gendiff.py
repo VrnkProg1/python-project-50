2a90745f1d5baff078c88aa27bf0b9f253bae86e
Vlasova Nika <vlasova_nika69@mail.ru>
2023-08-01 14:21:28 +0300

    remove undue files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,18 +1,60 @@
-from gendiff.difference_checker import generate_diff
-from fixtures.right import RIGHT_RESULT
+"""Gendiff tests"""
+
+from gendiff import generate_diff
+import pytest
+
+from fixtures.output import *
 
 
-def test_gendiff_json():
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
-
-def test_gendiff_yml():
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml')
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yml')
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yaml')
-
-def test_gendiff_yml_json():
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.yml')
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.json')
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.yaml', 'tests/fixtures/file2.json')
-	assert RIGHT_RESULT == generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.yaml')
+@pytest.mark.parametrize("first_file, second_file, expected_output, format", [
+    (
+        './tests/fixtures/yaml/filepath1_plain.yml',
+        './tests/fixtures/yaml/filepath2_plain.yml',
+        RIGHT_SIMPLE,
+        'stylish'
+    ),
+    (
+        './tests/fixtures/json/file1_plain.json',
+        './tests/fixtures/json/file2_plain.json',
+        RIGHT_SIMPLE,
+        'stylish'
+    ),
+    (
+        './tests/fixtures/json/file1_nest.json',
+        './tests/fixtures/json/file2_nest.json',
+        RIGHT_STYLISH,
+        'stylish'
+    ),
+    (
+        './tests/fixtures/yaml/filepath1_nest.yml',
+        './tests/fixtures/yaml/filepath2_nest.yml',
+        RIGHT_STYLISH,
+        'stylish'
+    ),
+    (
+        './tests/fixtures/json/file1_nest.json',
+        './tests/fixtures/json/file2_nest.json',
+        RIGHT_PLAIN,
+        'plain'
+    ),
+    (
+        './tests/fixtures/yaml/filepath1_nest.yml',
+        './tests/fixtures/yaml/filepath2_nest.yml',
+        RIGHT_PLAIN,
+        'plain'
+    ),
+    (
+        './tests/fixtures/json/file1_nest.json',
+        './tests/fixtures/json/file2_nest.json',
+        RIGHT_JSON,
+        'json'
+    ),
+    (
+        './tests/fixtures/yaml/filepath1_nest.yml',
+        './tests/fixtures/yaml/filepath2_nest.yml',
+        RIGHT_JSON,
+        'json'
+    ),
+])
+def test_gendiff(first_file, second_file, expected_output, format):
+    assert generate_diff(first_file, second_file, format) == expected_output
